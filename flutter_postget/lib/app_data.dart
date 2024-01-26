a58e6bf6d15836391a29c4c258a4a689c82a43e5
jordi <jorlosnav@gmail.com>
2024-01-26 16:17:30 +0100

    modified:   flutter_postget/lib/app_data.dart

--- flutter_postget/lib/app_data.dart
+++ flutter_postget/lib/app_data.dart
@@ -119,4 +119,10 @@ class AppData with ChangeNotifier {
     loadingPost = false;
     notifyListeners();
   }
+
+  String fileToBase64(File file) {
+    List<int> fileBytes = file.readAsBytesSync();
+    String base64Image = base64Encode(fileBytes);
+    return base64Image;
+  }
 }
